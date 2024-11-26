4f63eb546dc4b776153c50ef900652ccaa924d49
charliewlkan <charliewlkan@gmail>
2024-11-26 12:25:20 +0000

    added articles and select article by id

---
+++ controllers/articles.controller.js
@@ -0,0 +1,21 @@
+const { selectArticleById } = require("../models/articles.model")
+
+const getArticleById = (req, res, next) => {
+  const { article_id } = req.params
+
+  if(isNaN(article_id)) {
+    return res.status(400).send({msg: "Invalid article ID"})
+  }
+
+  selectArticleById(article_id).then((article) => {
+    res.status(200).send({article})
+  }).catch((err) => {
+    if(err.status) {
+      res.status(err.status).send({msg: err.msg})
+    } else {
+      next(err)
+    }
+  })
+}
+
+module.exports = {getArticleById}
