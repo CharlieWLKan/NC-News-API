8beb6932779b9bccc892120ff37e4353c41a3899
charliewlkan <charliewlkan@gmail>
2024-11-28 10:38:16 +0000

    added get users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -317,3 +317,24 @@ describe("DELETE /api/comments/:comment_id", () => {
       })
   })
 })
+
+describe("GET /api/users", () => {
+  test("200: responds with an array of users with correct properties", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        const { users } = body;
+        expect(users).toBeInstanceOf(Array);
+        users.forEach((user) => {
+          expect(user).toEqual(
+            expect.objectContaining({
+              username: expect.any(String),
+              name: expect.any(String),
+              avatar_url: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
