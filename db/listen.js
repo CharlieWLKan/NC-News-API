cf0a18a7df5b5b08b4a68c7c28f66771aff1e46f
charliewlkan <charliewlkan@gmail>
2024-11-28 11:12:58 +0000

    starting server

--- db/listen.js
+++ db/listen.js
@@ -1,5 +1,5 @@
 const app = require("../app")
 
-app.listen("8000", () => {
-  console.log("port listening")
-})
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
